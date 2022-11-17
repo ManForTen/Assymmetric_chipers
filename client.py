ba7e802dc356d7f6f37cc56303691c4908bd2778
Artem Petrikov <manforten@tandex.ru>
2022-11-17 21:05:08 +0300

    new changes

--- client.py
+++ client.py
@@ -1,14 +1,30 @@
+from DH_class import DH_Endpoint
 import socket
-import pickle
 
-HOST = '127.0.0.1'
-PORT = 8080
+HOST = 'localhost'
+PORT = 8132
 
 sock = socket.socket()
 sock.connect((HOST, PORT))
-
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
-
-sock.close()
+clientDH = DH_Endpoint() # Вызов класса
+clientDH.all_of_public_keys() # Создание ключей
+keys = str(clientDH.client_public_key)+' '+str(clientDH.server_public_key)
+sock.send(keys.encode()) # Отправление публичных ключей серверу
+msg = sock.recv(1024).decode()
+if msg == "Успешное подключение!":
+    print(msg)
+    print("Для выхода напишите 'Выход'")
+    server_key_partial = int(sock.recv(1024).decode())
+    client_partial_key = clientDH.generate_partial_key()
+    sock.send(str(client_partial_key).encode())  # Отправка частичного ключа клиента (А) серверу
+    clientDH.generate_full_key(server_key_partial)
+    while True:
+        msg = input("Введите сообщение:")
+        if msg == 'Выход' or msg == 'выход':
+            sock.send(clientDH.encrypt_message(msg).encode())
+            break
+        sock.send(clientDH.encrypt_message(msg).encode()) # Отправка закодированного сообщения
+    sock.close()
+else:
+    print("Вы не смогли подключится")
+    sock.close()
--- server.py
+++ server.py
@@ -1,15 +1,48 @@
+from DH_class import DH_Endpoint
+import random
 import socket
-import pickle
 
-HOST = '127.0.0.1'
-PORT = 8080
+HOST = 'localhost'
+PORT = 8132
 
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+print('Прослушивается данный порт:',PORT)
 
-conn.close()
+def make_keys(conn):
+    L = conn.recv(2054).decode()
+    L = L.split(' ')
+    serverDH = DH_Endpoint(int(L[0]), int(L[1]), random.randint(1, 320))
+    return serverDH
+
+def access_check(client_public_key): # Проверка на нахождение в публичного ключа в Keys.txt
+    with open('Keys.txt', 'r') as file:
+        flag = False
+        for line in file:
+            if int(line) == client_public_key:
+                flag = True
+                break
+    return flag
+
+serverDH = make_keys(conn)
+
+if access_check(serverDH.client_public_key):
+    conn.send("Успешное подключение!".encode())
+    server_partial_key = serverDH.generate_partial_key()
+    conn.send(str(server_partial_key).encode())
+    client_key_partial = int(conn.recv(1024).decode()) # Частичный ключ от клиента
+    serverDH.generate_full_key(client_key_partial) # Cоздание полного ключа
+    print('----------------------------------------')
+    while True:
+        msg = conn.recv(2024).decode()
+        print('Зашифрованное сообщение:',msg)
+        print('Расшифрованное сообщение:',serverDH.decrypt_message(msg))
+        print('----------------------------------------')
+        if serverDH.decrypt_message(msg) == 'Выход' or serverDH.decrypt_message(msg) == 'выход':
+            break
+    conn.close()
+else:
+    conn.close()
